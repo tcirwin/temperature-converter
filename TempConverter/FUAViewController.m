a924d6f10022266cb10ba54d403bc658d10818dc
Therin Irwin <therin.irwin@gmail.com>
2013-10-04 22:57:30 -0700

    Wire up fields and button for temp converter

--- TempConverter/FUAViewController.m
+++ TempConverter/FUAViewController.m
@@ -10,6 +10,12 @@
 
 @interface FUAViewController ()
 
+@property (assign, nonatomic) BOOL isFToC;
+@property (weak, nonatomic) IBOutlet UITextField *cTextField;
+@property (weak, nonatomic) IBOutlet UITextField *fTextField;
+
+- (IBAction)onTap:(UITapGestureRecognizer *)sender;
+
 @end
 
 @implementation FUAViewController
@@ -19,11 +25,39 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)cValueChanged:(id)sender {
+    _isFToC = NO;
+    [self convert];
+}
+- (IBAction)fValueChanged:(id)sender {
+    _isFToC = YES;
+    [self convert];
+}
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)convertButtonPressed:(id)sender {
+}
 
+- (IBAction)onTap:(UITapGestureRecognizer *)sender {
+    [self.view endEditing:YES];
+}
+
+- (void)convert {
+    float startTemp;
+    float endTemp;
+    
+    if (_isFToC) {
+        startTemp = _fTextField.text.floatValue;
+        endTemp = (startTemp - 32) * (5.0/9);
+        _cTextField.text = [NSString stringWithFormat:@"%.1f", endTemp];
+    } else {
+        startTemp = _cTextField.text.floatValue;
+        endTemp = ((9.0/5) * startTemp) + 32;
+        _fTextField.text = [NSString stringWithFormat:@"%.1f", endTemp];
+    }
+}
 @end
